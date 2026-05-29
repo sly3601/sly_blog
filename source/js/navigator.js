18b3d3b42a2fe55f9cf09aa0de7cb5413645d19e
DR_WU <1967584065@qq.com>
2026-05-30 01:30:59 +0800

    Unify navigator glass icons

--- source/js/navigator.js
+++ source/js/navigator.js
@@ -444,7 +444,11 @@
   function renderIcon(host, site) {
     host.innerHTML = '';
     host.classList.remove('is-favicon-icon', 'is-image-icon', 'is-text-icon');
-    host.style.backgroundColor = site.iconType === 'favicon' || site.iconType === 'image' ? 'transparent' : site.color;
+    if (site.iconType === 'favicon' || site.iconType === 'image') {
+      host.style.removeProperty('background-color');
+    } else {
+      host.style.backgroundColor = site.color;
+    }
     host.style.color = readableTextColor(site.color);
 
     if (site.iconType === 'favicon' || site.iconType === 'image') {
@@ -640,14 +644,10 @@
   function brandIconUrls(host) {
     const normalizedHost = String(host || '').toLowerCase();
     if (normalizedHost === 'bilibili.com' || normalizedHost.endsWith('.bilibili.com')) {
-      return [svgDataUrl(`
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
-          <path d="M22.7 19.8 16.9 14l3-3 6.8 6.7h10.6l6.8-6.7 3 3-5.8 5.8c4.9.8 7.9 4.4 7.9 9.6v12c0 6.3-4.1 10-11 10H25.8c-6.9 0-11-3.7-11-10v-12c0-5.2 3-8.8 7.9-9.6Z" fill="none" stroke="#13b5e9" stroke-width="5.2" stroke-linejoin="round"/>
-          <circle cx="25.2" cy="35" r="2.7" fill="#13b5e9"/>
-          <circle cx="38.8" cy="35" r="2.7" fill="#13b5e9"/>
-          <path d="M27 43.3h10" stroke="#13b5e9" stroke-width="4.3" stroke-linecap="round"/>
-        </svg>
-      `)];
+      return [
+        'https://cdn.simpleicons.org/bilibili/00A1D6',
+        svgDataUrl('<svg fill="#00A1D6" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Bilibili</title><path d="M17.813 4.653h.854c1.51.054 2.769.578 3.773 1.574 1.004.995 1.524 2.249 1.56 3.76v7.36c-.036 1.51-.556 2.769-1.56 3.773s-2.262 1.524-3.773 1.56H5.333c-1.51-.036-2.769-.556-3.773-1.56S.036 18.858 0 17.347v-7.36c.036-1.511.556-2.765 1.56-3.76 1.004-.996 2.262-1.52 3.773-1.574h.774l-1.174-1.12a1.234 1.234 0 0 1-.373-.906c0-.356.124-.658.373-.907l.027-.027c.267-.249.573-.373.92-.373.347 0 .653.124.92.373L9.653 4.44c.071.071.134.142.187.213h4.267a.836.836 0 0 1 .16-.213l2.853-2.747c.267-.249.573-.373.92-.373.347 0 .662.151.929.4.267.249.391.551.391.907 0 .355-.124.657-.373.906zM5.333 7.24c-.746.018-1.373.276-1.88.773-.506.498-.769 1.13-.786 1.894v7.52c.017.764.28 1.395.786 1.893.507.498 1.134.756 1.88.773h13.334c.746-.017 1.373-.275 1.88-.773.506-.498.769-1.129.786-1.893v-7.52c-.017-.765-.28-1.396-.786-1.894-.507-.497-1.134-.755-1.88-.773zM8 11.107c.373 0 .684.124.933.373.25.249.383.569.4.96v1.173c-.017.391-.15.711-.4.96-.249.25-.56.374-.933.374s-.684-.125-.933-.374c-.25-.249-.383-.569-.4-.96V12.44c0-.373.129-.689.386-.947.258-.257.574-.386.947-.386zm8 0c.373 0 .684.124.933.373.25.249.383.569.4.96v1.173c-.017.391-.15.711-.4.96-.249.25-.56.374-.933.374s-.684-.125-.933-.374c-.25-.249-.383-.569-.4-.96V12.44c.017-.391.15-.711.4-.96.249-.249.56-.373.933-.373Z"/></svg>')
+      ];
     }
     return [];
   }
